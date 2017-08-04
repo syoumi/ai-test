556a81f9f9534fdfa76e9e1d05ef0c8b05c91997
mimokyop <mita.oumaima@gmail.com>
2017-08-04 11:45:29 +0100

    Get distinct

--- functions/getPercent.js
+++ functions/getPercent.js
@@ -1,5 +1,21 @@
 
 
 var getPercent= (words, keywords)=> {
-  
+
+  var counter = 0;
+
+  words.forEach((word) => {
+    keywords.forEach((keyword) => {
+      if (word === keyword.toLowerCase().trim()) {
+        counter++;
+      }
+    });
+  });
+
+
+}
+
+
+module.exports= {
+  getPercent
 }
